d65e6b7b5435fb8c98b7309a026a54380a1e2da3
Rajashekar Chintalapati <rajashekar.chintalapati@walmart.com>
2023-08-18 14:11:46 -0700

    Check if given file is video or audio

--- video-to-text/text_extract_from_video.py
+++ video-to-text/text_extract_from_video.py
@@ -14,19 +14,31 @@ from moviepy.editor import VideoFileClip
 from pydub import AudioSegment
 import openai
 
-
 # Get video path from arguments
 video_clip_path = sys.argv[1]
 v_path = ' '.join(video_clip_path.split(".")[:-1])
 
 # Step 1: Extract Audio from Video
+video_clip = None
 print("Extracting audio from video")
-video_clip = VideoFileClip(video_clip_path)
-print(f"Video has {video_clip.duration // 60} minutes")
+try:
+    video_clip = VideoFileClip(video_clip_path)
+    print(f"Video has {video_clip.duration // 60} minutes")
+except Exception as e:
+    print("its not a video file")
+if video_clip is None and video_clip_path.split(".")[-1] == "mp4":
+    print("trying to extract audio from mp4 file")
+    audio = AudioSegment.from_file(video_clip_path, "mp4")
+    audio.export(f"{v_path}.mp3", format="mp3")
+else:
+    print(f"it is not a mp4 file, it is a {video_clip_path.split('.')[-1]} file")
 
 # Step 2: Save Audio to a mp3 file
-print("Saving audio to mp3 file")
-video_clip.audio.write_audiofile(f"{v_path}.mp3")
+if video_clip and not os.path.exists(f"{v_path}.mp3"):
+    print("Saving audio to mp3 file")
+    video_clip.audio.write_audiofile(f"{v_path}.mp3")
+else:
+    print("Audio file already exists")
 
 # Step 3: Check duration of audio file
 audio = AudioSegment.from_mp3(f"{v_path}.mp3")
